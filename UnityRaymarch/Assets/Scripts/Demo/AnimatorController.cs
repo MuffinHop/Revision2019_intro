0e23ec7c3f7145cd6ff414382d247e205db7ec2d
sampo <sampo.savilampi@gmail.com>
2019-04-14 16:59:37 +0300

    reverted rocket.

--- UnityRaymarch/Assets/Scripts/Demo/AnimatorController.cs
+++ UnityRaymarch/Assets/Scripts/Demo/AnimatorController.cs
@@ -25,6 +25,10 @@ public class AnimatorController : MonoBehaviour
         }
 
         _animator.speed = 0;
-        _animator.Play(""+_animid, -1, SyncUp.GetVal("Animator Time " + gameObject.name));
+        _animator.Play(""+_animid, -1, SyncUp.GetVal("Animator Time " + gameObject.name) % 1.0f);
+
+
+        Vector3 position = new Vector3(SyncUp.GetVal("Position X" + gameObject.name), SyncUp.GetVal("Position Y" + gameObject.name), SyncUp.GetVal("Position Z" + gameObject.name));
+        transform.position = position;
     }
 }
