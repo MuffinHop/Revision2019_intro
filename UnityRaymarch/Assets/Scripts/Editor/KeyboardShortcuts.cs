ae11d2ed413e6a050f46e3dd9c7033a610cdcd1d
Visa-Valtteri Pimiä <vp79799@gmail.com>
2019-04-27 20:34:06 +0300

    added screenshot key f12

--- UnityRaymarch/Assets/Scripts/Editor/KeyboardShortcuts.cs
+++ UnityRaymarch/Assets/Scripts/Editor/KeyboardShortcuts.cs
@@ -10,4 +10,13 @@ public class KeyboardShortcuts : ScriptableObject {
         RayMarchingController rm = GameObject.Find("Main Camera").GetComponent<RayMarchingController>();
         if (rm.GeneratedOnce) rm.RegenerateShader();
     }
+    [MenuItem("Edit/Screenshot _F12")]
+    static void TakeScreenshot()
+    {
+        string path = EditorUtility.SaveFilePanel("Take screenshot", "", "capture.png", "png");
+        if (path.Length != 0)
+        {
+            ScreenCapture.CaptureScreenshot(path, 1); 
+        }
+    }
 }
