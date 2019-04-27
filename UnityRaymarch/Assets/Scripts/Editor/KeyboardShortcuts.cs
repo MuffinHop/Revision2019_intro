bebdb8bb063910ec12bdd29315a46f3d262b75c6
Visa-Valtteri Pimiä <vp79799@gmail.com>
2019-04-27 19:50:15 +0300

    live reloading of shader assets, and f5 to force regeneration of shader

---
+++ UnityRaymarch/Assets/Scripts/Editor/KeyboardShortcuts.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class KeyboardShortcuts : ScriptableObject {
+
+    [MenuItem("Edit/Refresh Shaders _F5")]
+    static void RefreshShaders() {
+        RayMarchingController rm = GameObject.Find("Main Camera").GetComponent<RayMarchingController>();
+        if (rm.GeneratedOnce) rm.RegenerateShader();
+    }
+}
---
+++ UnityRaymarch/Assets/Scripts/Editor/ShaderPostprocessor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+class ShaderPostprocessor : AssetPostprocessor
+{
+    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        foreach (string str in importedAssets)
+        {
+            if (str.Contains("Shaders") && !str.Contains("Megashader"))
+            {
+                RayMarchingController rm = GameObject.Find("Main Camera").GetComponent<RayMarchingController>();
+                if (rm.GeneratedOnce) rm.RegenerateShader();
+            }
+        }
+    }
+}
